949cc8e373845c577715f01e43cb6e6f4b56b0df
rodgerswisdom <trulyhawona@gmail.com>
2025-02-18 23:00:57 +0300

    Test Put

--- routes.js
+++ routes.js
@@ -7,13 +7,14 @@ const router = express.Router();
 const book = require('./db');
 
 router.get('/items', async (req, res) => {
-    try{
+    try {
         const myBooks = await book.find();
-        res.status(200).send(myBooks);
-    } catch(error) {
-        res.status(404).send(`Resource not Found ${error}`);
+        res.status(200).json(myBooks);
+    } catch (error) {
+        res.status(404).json(`Resource not Found ${error}`);
     }
 });
+
 
 router.post('/items', async (req, res) => {
     try{
@@ -27,6 +28,28 @@ router.post('/items', async (req, res) => {
     } catch(error){
         res.status(500).send(`Error creating book${error}`);
 }
+});
+
+router.put('/items/:id', async (req, res) => {
+    try {
+        const { title, author, pages } = req.body;
+        if (!title || !author || !pages) {
+            return res.status(400).send("All fields are required");
+        }
+        const updateBook = await book.findByIdAndUpdate(req.params.id, { title, author, pages }, { new: true });
+        res.status(200).send(`Book Updated: ${updateBook}`);
+    } catch (error) {
+        res.status(500).send(`Error updating book ${error}`);
+    }
+});
+
+router.delete('items/:id', async (req, res) => {
+    try{
+        const deleteBook = await book.findByIdAndDelete(req.params.id);
+        res.status(200).send(`Book Deleted: ${deleteBook}`);
+    } catch(error){
+        res.status(500).send(`Error deleting book ${error}`);
+    }
 });
     
 module.exports = router;
